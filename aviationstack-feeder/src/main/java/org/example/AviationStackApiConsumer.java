0d1357fc68967eb7045d4f64e3a9dcb1fae1fcc6
AyozeRuano <ayozeruanoalcantara@gmail.com>
2025-03-20 11:55:37 +0000

    Jsoup añadido en cada pom Version JDK --> 21 AviationStackApiConsumer v1

---
+++ aviationstack-feeder/src/main/java/org/example/AviationStackApiConsumer.java
@@ -0,0 +1,23 @@
+package org.example;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+
+public class AviationStackApiConsumer {
+    public static void main(String[] args) {
+        try{
+            String endpoint = "https://api.aviationstack.com/v1/flights";
+            Connection connection = Jsoup.connect(endpoint);
+            connection.ignoreContentType(true);
+            connection.data("access_key", "66c5fafcb31f116b9ee7dce896c47d3c");
+            connection.data("limit", "3");
+
+            Connection.Response response = connection.method(Connection.Method.GET).execute();
+            System.out.println(response.body());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
